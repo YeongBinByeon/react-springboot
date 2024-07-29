0546e4f731d8bcd2f05f158ecdc8109182f81a17
yeongbin <yeongbin6483@gmail.com>
2024-07-29 16:58:51 +0900

    섹션 2 / 서비스계층 만들기

--- backend/apiserver/src/test/java/org/zerock/apiserver/service/TodoServiceTests.java
+++ backend/apiserver/src/test/java/org/zerock/apiserver/service/TodoServiceTests.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.apiserver.dto.TodoDTO;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 @Log4j2
@@ -17,5 +20,18 @@ public class TodoServiceTests {
         Long tno = 50L;
 
         log.info(todoService.get(tno));
+    }
+
+    @Test
+    public void testRegister(){
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("Title...")
+                .content("Content......")
+                .dueDate(LocalDate.of(2023,12,31))
+                .build();
+
+        log.info(todoService.register(todoDTO));
+
     }
 }
