8caf612d6e3a00c51cb676130890f7fd63157ebe
yeongbin <yeongbin6483@gmail.com>
2024-08-07 15:22:47 +0900

    섹션 4 / 서비스계층과 컨트롤러(1) - 상품 목록/등록

--- backend/apiserver/src/test/java/org/zerock/apiserver/service/ProductServiceTests.java
+++ backend/apiserver/src/test/java/org/zerock/apiserver/service/ProductServiceTests.java
@@ -8,6 +8,9 @@ import org.zerock.apiserver.dto.PageRequestDTO;
 import org.zerock.apiserver.dto.PageResponseDTO;
 import org.zerock.apiserver.dto.ProductDTO;
 
+import java.util.List;
+import java.util.UUID;
+
 @SpringBootTest
 @Log4j2
 public class ProductServiceTests {
@@ -22,5 +25,22 @@ public class ProductServiceTests {
         PageResponseDTO<ProductDTO> responseDTO = productService.getList(pageRequestDTO);
 
         log.info(responseDTO.getDtoList());
+    }
+
+    @Test
+    public void testRegister(){
+        ProductDTO productDTO = ProductDTO.builder()
+                .pname("새로운 상품")
+                .pdesc("신규 추가 상품입니다")
+                .price(1000)
+                .build();
+
+        //uuid가 있어야 함
+        productDTO.setUploadFileNames(
+                List.of(UUID.randomUUID()+"_"+"Test1.jpg",
+                        UUID.randomUUID()+"_"+"Test2.jpg")
+        );
+
+        productService.register(productDTO);
     }
 }
