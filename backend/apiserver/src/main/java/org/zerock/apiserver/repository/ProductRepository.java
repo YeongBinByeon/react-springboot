0e11566088590a68b496d5ceb04bada49fffcc3c
yeongbin <yeongbin6483@gmail.com>
2024-08-07 13:55:36 +0900

    섹션 4 / Querydsl에서 @ElementCollection 처리

--- backend/apiserver/src/main/java/org/zerock/apiserver/repository/ProductRepository.java
+++ backend/apiserver/src/main/java/org/zerock/apiserver/repository/ProductRepository.java
@@ -1,15 +1,18 @@
 package org.zerock.apiserver.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.apiserver.domain.Product;
+import org.zerock.apiserver.repository.search.ProductSearch;
 
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
 
     // 엘리먼트 엔티티 그래프 - 이걸 쓰면 연관된 엔티티를 같이 조인해서 로딩을 한다.
     @EntityGraph(attributePaths = "imageList")
@@ -20,4 +23,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
     void updateToDelete( @Param("pno") Long pno, @Param("delFlag") boolean flag);
 
+    @Query("select p, pi from Product p left join p.imageList pi where pi.ord = 0 and p.delFlag = false")
+    Page<Object[]> selectList(Pageable pageable);
 }
--- backend/apiserver/src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
+++ backend/apiserver/src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
@@ -5,9 +5,15 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.zerock.apiserver.domain.Product;
+import org.zerock.apiserver.dto.PageRequestDTO;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,16 +26,19 @@ public class ProductRepositoryTests {
 
     @Test
     public void testInsert(){
-        Product product = Product.builder()
-                .pname("Test ")
-                .pdesc("Test Desc")
-                .price(1000)
-                .build();
 
-        product.addImageString(UUID.randomUUID()+"_"+"IMAGE1.jpg");
-        product.addImageString(UUID.randomUUID()+"_"+"IMAGE2.jpg");
+        for(int i=0; i<10; i++) {
+            Product product = Product.builder()
+                    .pname("Test ")
+                    .pdesc("Test Desc")
+                    .price(1000)
+                    .build();
 
-        productRepository.save(product);
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+
+            productRepository.save(product);
+        }
     }
 
 //    @Test
@@ -82,6 +91,22 @@ public class ProductRepositoryTests {
         product.addImageString(UUID.randomUUID()+"_"+"PIMAGE3.jpg");
 
         productRepository.save(product);
+    }
+
+    @Test
+    public void testList(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("pno").descending());
+
+        Page<Object[]> result = productRepository.selectList(pageable);
+
+        result.getContent().forEach(arr -> log.info(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void testSearch(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
+
+        productRepository.searchList(pageRequestDTO);
     }
 
 }
