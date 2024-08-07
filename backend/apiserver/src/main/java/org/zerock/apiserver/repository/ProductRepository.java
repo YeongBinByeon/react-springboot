1b11e080c7f849b4b38ff37712f224a82a1a8f43
yeongbin <yeongbin6483@gmail.com>
2024-08-07 11:51:23 +0900

    섹션 4 / 엔티티 CRUD 처리

--- backend/apiserver/src/main/java/org/zerock/apiserver/repository/ProductRepository.java
+++ backend/apiserver/src/main/java/org/zerock/apiserver/repository/ProductRepository.java
@@ -1,7 +1,23 @@
 package org.zerock.apiserver.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.apiserver.domain.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // 엘리먼트 엔티티 그래프 - 이걸 쓰면 연관된 엔티티를 같이 조인해서 로딩을 한다.
+    @EntityGraph(attributePaths = "imageList")
+    @Query("select p from Product p where p.pno = :pno")
+    Optional<Product> selectOne(@Param("pno") Long pno);
+
+    @Modifying
+    @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
+    void updateToDelete( @Param("pno") Long pno, @Param("delFlag") boolean flag);
+
 }
--- backend/apiserver/src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
+++ backend/apiserver/src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
@@ -1,11 +1,14 @@
 package org.zerock.apiserver.repository;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.zerock.apiserver.domain.Product;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -25,6 +28,58 @@ public class ProductRepositoryTests {
 
         product.addImageString(UUID.randomUUID()+"_"+"IMAGE1.jpg");
         product.addImageString(UUID.randomUUID()+"_"+"IMAGE2.jpg");
+
+        productRepository.save(product);
+    }
+
+//    @Test
+//    public void testRead(){
+//
+//        Long pno = 1L;
+//
+//        Optional<Product> result = productRepository.findById(pno);
+//
+//        Product product = result.orElseThrow();
+//
+//        log.info(product);
+//
+//        log.info(product.getImageList());
+//    }
+
+    @Test
+    public void testRea2(){
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete(){
+        Long pno = 2L;
+
+        productRepository.updateToDelete(2L, true);
+    }
+
+    @Test
+    public void testUpdate(){
+        Product product = productRepository.selectOne(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID()+"_"+"PIMAGE1.jpg");
+        product.addImageString(UUID.randomUUID()+"_"+"PIMAGE2.jpg");
+        product.addImageString(UUID.randomUUID()+"_"+"PIMAGE3.jpg");
 
         productRepository.save(product);
     }
