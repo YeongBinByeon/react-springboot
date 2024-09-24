ddc0efddd6da1133fabfaecb8bd6890feac14cf8
yeongbin <yeongbin6483@gmail.com>
2024-09-24 13:59:23 +0900

    JWT 체크 필터 만들기

--- backend/apiserver/src/main/java/org/zerock/apiserver/config/CustomSecurityConfig.java
+++ backend/apiserver/src/main/java/org/zerock/apiserver/config/CustomSecurityConfig.java
@@ -9,9 +9,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerock.apiserver.security.filter.JWTCheckFilter;
 import org.zerock.apiserver.security.handler.APILoginFailHandler;
 import org.zerock.apiserver.security.handler.APILoginSuccessHandler;
 
@@ -43,6 +45,8 @@ public class CustomSecurityConfig {
                     config.failureHandler(new APILoginFailHandler());
                 }
         );
+
+        http.addFilterBefore(new JWTCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
